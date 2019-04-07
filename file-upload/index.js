b4471a59618577f3de77f85f469f3b2582bd629f
parkoon <par_koon@naver.com>
2019-04-07 23:33:37 +0900

    파일 업로드

--- file-upload/index.js
+++ file-upload/index.js
@@ -1,38 +1,65 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const mkdir = require("mkdirp");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
-const multer = require('multer')
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const { getToday } = require('./utils/date')
-console.log(getToday())
+const multer = require("multer");
+
+const { getToday } = require("./utils/date");
+console.log(getToday());
+
+app.use(express.static("uploads"));
+
 const imageStorage = multer.diskStorage({
-    filename: (req, fiel, callback) => {
-        callback(null, `uploads/id/${getToday()}`)
-    },
     destination: (req, file, callback) => {
+        let des = path.join(
+            __dirname,
+            `uploads/${req.params.userId}/${getToday()}`
+        );
+        let isDirExists = fs.existsSync(des) && fs.lstatSync(des).isDirectory();
 
+        if (!isDirExists) mkdir.sync(des);
+
+        req.filepath = `${req.params.userId}/${getToday()}`;
+
+        callback(null, des);
+    },
+    filename: (req, file, callback) => {
+        const filename = `${Date.now()}-${file.originalname}`;
+        req.filename = filename;
+        callback(null, filename);
     }
-})
+});
 
-const upload = multer({ dest: 'uploads/' })
+const imageMiddleware = multer({ storage: imageStorage });
 
-const path = require('path')
+const path = require("path");
 
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
 
+app.get("/upload", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+app.post("/upload/image/:userId", imageMiddleware.array("images", 3), function(
+    req,
+    res
+) {
+    const { filepath, filename } = req;
+    console.log("여긴 타겠지", req.filename, req.filepath);
 
-app.post('/upload/image', upload.single('image'), function (req, res) {
-    res.send('image Uploaded! : ' + req.file); // object를 리턴함
-    console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
-})
+    res.status(200).json({
+        filepath: `http://localhost:3001/${filepath}/${filename}`
+    });
+});
 
-app.post('/upload/record', upload.single('record'), function (req, res) {
-    res.send('record Uploaded! : ' + req.file); // object를 리턴함
-    console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
-})
-
-
-app.listen('3001')
+app.listen("3001");
